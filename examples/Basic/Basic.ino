de65922339e9a0c53a97b509c94a3fe2088f8c48
zocker007 <merlin.bromberg@gmx.net>
2023-05-21 12:44:37 +0200

    removed code from basic example that has no relation to this lib

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,22 +1,16 @@
-#include <G110.h>
-#include <USS.h>
-
 #include <Wire.h>
 #include "Nunchuk.h"
 
 using namespace communication;
 Nunchuk dev{Control::ADDR_NUNCHUK};
 
-constexpr const uint8_t PIN_LVLSHFT_DI2C{10};
 constexpr const uint8_t PIN_LVLSHFT_NUNCHUK{11};
 
 void setup()
 {
   // Pegelwandler initialisieren
   pinMode(PIN_LVLSHFT_NUNCHUK, OUTPUT);
-  pinMode(PIN_LVLSHFT_DI2C, OUTPUT);
   digitalWrite(PIN_LVLSHFT_NUNCHUK, HIGH);
-  digitalWrite(PIN_LVLSHFT_DI2C, LOW);
   
   dev.libinit();
   serialinfo("Nunchuk-Pegelwandler aktiviert. DI2C-Pegelwandler deaktiviert.");
