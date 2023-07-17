a8f924bee023c46089f32f8981d359ebcf1f1a3f
zocker007 <merlin.bromberg@gmx.net>
2023-07-17 20:45:31 +0200

    adapt basic example

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -3,12 +3,13 @@
 
 using namespace communication;
 constexpr const uint8_t PIN_LVLSHFT_NUNCHUK{11};
-Nunchuk dev{100, 50, ClockMode::I2C_CLOCK_FAST_400_kHz};
+Nunchuk dev{PIN_LVLSHFT_NUNCHUK, 100, 50, ClockMode::I2C_CLOCK_FAST_400_kHz};
 
 
 void setup()
 {
   Serial.begin(115200);
+  delay(3000);
   Serial.println("Serieller Monitor initialisiert");
 
   // Nunchuk initialisieren
@@ -17,11 +18,13 @@ void setup()
 
 void loop()
 {
-  Serial.println();
-
   // Messwerte auslesen
-  dev.read();
+  if(dev.read() != State::NO_DATA_AVAILABLE)
+  {
+    Serial.println();
+    // Messwerte auslesen
+    dev.print();
+  }
 
-  // Messwerte auslesen
-  dev.print();
+  delayMicroseconds(1000);
 }
